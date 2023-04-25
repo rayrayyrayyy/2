43ed2d5af0d944fa057a98b12b9d81218b0ab5a7
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-25 18:23:56 +0800

    pseudocode

---
+++ multiple lines.py
@@ -0,0 +1,12 @@
+# Ray Allessandra D. Pacis | BSCPE 1-5 
+# Write a method in python to write multiple line of text contents into a text file mylife.txt. 
+
+# open mylife.txt(write)
+    # create loop
+        # ask user for input
+        # write user's input to mylife.txt
+        # ask the user if they want to add more lines
+        # write user's input to mylife.txt
+        # if yes:
+        # if no:
+        # if user input is invalid
