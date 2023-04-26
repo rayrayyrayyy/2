f9c30668d4fea03123acc801aa84dfefddedf7a6
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-26 15:19:49 +0800

    design

--- multiple lines.py
+++ multiple lines.py
@@ -5,6 +5,7 @@ import pyfiglet
 from termcolor import colored, cprint
 
 print_yellow = lambda x: cprint(x, 'yellow')
+print_red = lambda x: cprint(x, 'red')
 
 # open mylife.txt(write)
 with open("mylife.txt", 'w') as input_file:
@@ -12,22 +13,23 @@ with open("mylife.txt", 'w') as input_file:
     # create loop
     while True:
         # ask user for input
-        user_input = input("Enter something: ")
+        user_input = input("\nEnter something: ")
         # write user's input to mylife.txt
-        input_file.write("Enter something: " + str(user_input) + '\n')
+        input_file.write("\nEnter something: " + str(user_input) + '\n')
         # ask the user if they want to add more lines
         reply = input("\n DO YOU WANT TO PUT ANOTHER LINE? YES/No? ")
 
         # write user's input to mylife.txt
         input_file.write("DO YOU WANT TO PUT ANOTHER LINE? YES/No? " + str(reply) + '\n')
         # if yes:
-        if reply == "Y" or reply == "Yes" or reply == "YES" or reply == "yes":
+        if reply == "Y" or reply == "y" or reply == "Yes" or reply == "YES" or reply == "yes":
              continue
         # if no:
-        if reply == "N" or reply == "No" or reply == "NO" or reply == "no":
-                exit("\n\tHave a great day!")
+        if reply == "N" or reply == "n" or reply == "No" or reply == "NO" or reply == "no":
+                exit(cprint("\n\tHave a great day!", "yellow"))
+                
         # if user input is invalid
         else:
-            print("\tYou can type either YES, Yes, yes, Y or NO, No, no, N.") #These are the only acceptable inputs
+            print_red("\tYou can type either YES, Yes, yes, Y or NO, No, no, N.") #These are the only acceptable inputs
             break
 # end of program
