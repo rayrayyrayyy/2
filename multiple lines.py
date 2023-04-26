ef4087c9c63b85fb472e79a4957f42fde90e8a4d
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-26 20:03:44 +0800

    hello

--- multiple lines.py
+++ multiple lines.py
@@ -6,6 +6,12 @@ from termcolor import colored, cprint
 
 print_yellow = lambda x: cprint(x, 'yellow')
 print_red = lambda x: cprint(x, 'red')
+print_magenta = lambda x: cprint(x, 'magenta')
+
+print_yellow('\n' + '-' * 100)
+intro = pyfiglet.figlet_format("Hello User!", font = 'contrast', width = 100, justify = 'center')
+print_magenta(intro)
+print_yellow('-' * 100)
 
 # open mylife.txt(write)
 with open("mylife.txt", 'w') as input_file:
@@ -14,6 +20,10 @@ with open("mylife.txt", 'w') as input_file:
     while True:
         # ask user for input
         user_input = input("\nEnter something: ")
+        import time
+        print ("...")
+        time.sleep (2)
+
         # write user's input to mylife.txt
         input_file.write("Enter something: " + str(user_input) + '\n')
         # ask the user if they want to add more lines
@@ -23,7 +33,7 @@ with open("mylife.txt", 'w') as input_file:
         input_file.write("\nDO YOU WANT TO PUT ANOTHER LINE? YES/No? " + str(reply) + '\n')
         # if yes:
         if reply == "Y" or reply == "y" or reply == "Yes" or reply == "YES" or reply == "yes":
-             continue
+            continue
         # if no:
         if reply == "N" or reply == "n" or reply == "No" or reply == "NO" or reply == "no":
                 exit(cprint("\n\tHave a great day!", "yellow"))
@@ -32,6 +42,6 @@ with open("mylife.txt", 'w') as input_file:
         else:
             invalid_txt = "INVALID!"
             invalid = pyfiglet.figlet_format(invalid_txt, font = "starwars", width = 100, justify = "center")
-            print_red(invalid + "\t\tYou can type either YES, Yes, yes, Y or NO, No, no, N.") #These are the only acceptable inputs
+            print_red(invalid + "\t\tYou can only type either YES, Yes, yes, Y or NO, No, no, N.") #These are the only acceptable inputs
             break
 # end of program
