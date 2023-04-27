2156c2b2ca8414ea7797d5f37e719b800d41c24f
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-28 00:47:06 +0800

    try

--- multiple lines.py
+++ multiple lines.py
@@ -4,7 +4,7 @@
 import pyfiglet
 from termcolor import colored, cprint
 
-print_white = lambda x: cprint(x, 'white')
+print_green = lambda x: cprint(x, 'green')
 print_yellow = lambda x: cprint(x, 'yellow')
 print_red = lambda x: cprint(x, 'red')
 print_magenta = lambda x: cprint(x, 'magenta')
@@ -14,7 +14,7 @@ intro = pyfiglet.figlet_format("Hello User!", font = 'starwars', width = 100, ju
 print_magenta(intro)
 welcome = "You can enter multiple messages and I'll save it in a file for you. You're welcome!"
 welcome_center = welcome.center(100)
-print_white(welcome_center)
+print_green(welcome_center)
 print_yellow('-' * 100)
 
 # open mylife.txt(write)
@@ -23,7 +23,7 @@ with open("mylife.txt", 'w') as input_file:
     # create loop
     while True:
         # ask user for input
-        user_input = input("\nEnter something: ")
+        user_input = input("\033[0;32m" +"\nEnter something: \033[0m")
         import time
         print_magenta("...")
         time.sleep (2)
@@ -31,7 +31,7 @@ with open("mylife.txt", 'w') as input_file:
         # write user's input to mylife.txt
         input_file.write("Enter something: " + str(user_input) + '\n')
         # ask the user if they want to add more lines
-        reply = input("\n DO YOU WANT TO PUT ANOTHER LINE? YES/No? ")
+        reply = input('\033[1m' + "\n DO YOU WANT TO PUT ANOTHER LINE? YES/No? ")
 
         # write user's input to mylife.txt
         input_file.write("\nDO YOU WANT TO PUT ANOTHER LINE? YES/No? " + str(reply) + '\n')
@@ -40,12 +40,14 @@ with open("mylife.txt", 'w') as input_file:
             continue
         # if no:
         if reply == "N" or reply == "n" or reply == "No" or reply == "NO" or reply == "no":
-                exit(cprint("\n\tHave a great day!", "yellow"))
-                
+            exit(cprint('-' * 100 + "\n\tHave a great day!", "yellow"))
+            print_yellow('-' * 100)
+                    
         # if user input is invalid
         else:
             invalid_txt = "INVALID!"
             invalid = pyfiglet.figlet_format(invalid_txt, font = "starwars", width = 100, justify = "center")
             print_red(invalid + "\t\tYou can only type either YES, Yes, yes, Y or NO, No, no, N.") #These are the only acceptable inputs
-            break
+            continue
+
 # end of program
